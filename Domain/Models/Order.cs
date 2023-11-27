9c9c7d24915d3012aaa45f2097368173c760ad47
jw <304495@via.dk>
2023-11-27 10:51:07 +0100

    Order push

--- Domain/Models/Order.cs
+++ Domain/Models/Order.cs
@@ -1,6 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Domain.Models;
 
 public class Order
 {
-    
+    [Key]
+    public int OrderId { get; set; }
+    public DateOnly PurchaseDate { get; set; }
+    public string DeliveryMethod { get; set; }
+    public CustomerInf CustomerId { get; set; }
+
+    public List<Product> Products { get; set; } = new List<Product>();
+
+    public Order(DateOnly purchaseDate, string deliveryMethod, CustomerInf customerId, List<Product> products )
+    {
+        PurchaseDate = purchaseDate;
+        DeliveryMethod = deliveryMethod;
+        CustomerId = customerId;
+        Products = products;
+    }
+
+    public Order(){}
 }
