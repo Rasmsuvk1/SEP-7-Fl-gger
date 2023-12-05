58b58c0736dc8aa5d16f412334615c51df0ec1fb
Rasmsuvk1 <rasmusvk1@gmail.com>
2023-12-05 11:29:01 +0100

    Connection between blazor admin and web api

--- BlazorFlüggerAdmin/Program.cs
+++ BlazorFlüggerAdmin/Program.cs
@@ -1,12 +1,21 @@
+using HttpClients.ClientInterfaces;
+using HttpClients.Implementations;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using BlazorFlüggerAdmin.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped(
+    sp => 
+        new HttpClient { 
+            BaseAddress = new Uri("http://localhost:5075") 
+        }
+);
+builder.Services.AddScoped<IProductService, ProductHttpClient>();
+builder.Services.AddScoped<IOrderSerivce, OrderHttpClient>();
 
 var app = builder.Build();
 
@@ -15,7 +24,11 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseCors(x => x
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .SetIsOriginAllowed(origin => true) // allow any origin
+        .AllowCredentials());
 }
 
 app.UseHttpsRedirection();
