5b657a6ceeb21b77bc03f456e79f4637941147e0
Rasmsuvk1 <rasmusvk1@gmail.com>
2023-12-05 11:23:10 +0100

    Bug fix establishing the connection

--- BlazorFlüggerSep/Program.cs
+++ BlazorFlüggerSep/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped(
     sp => 
         new HttpClient { 
-            BaseAddress = new Uri("https://localhost:5075") 
+            BaseAddress = new Uri("http://localhost:5075") 
         }
 );
 builder.Services.AddScoped<IProductService, ProductHttpClient>();
