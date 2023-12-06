5b83e881564e0d4fbedcc65fa4531b157be5be70
Rasmsuvk1 <rasmusvk1@gmail.com>
2023-12-06 11:25:33 +0100

    Handle Special Letters?

--- HttpClients/Implementations/CustomerHttpClient.cs
+++ HttpClients/Implementations/CustomerHttpClient.cs
@@ -1,3 +1,4 @@
+
 using System.Net.Http.Json;
 using System.Text.Json;
 using Domain.DTOs.CustomerDTOS;
@@ -17,8 +18,15 @@ public class CustomerHttpClient : ICustomerService
 
     public async Task<CustomerInfo> CreateAsync(CustomerCreationDto dto)
     {
+        //Validate for special letters:
+        dto.Name = dataValidation(dto.Name);
+        dto.address = dataValidation(dto.address);
+        dto.Email = dataValidation(dto.Email);
+        dto.Tlf = dataValidation(dto.Tlf);
+        
         try
         {
+            
             HttpResponseMessage response = await client.PostAsJsonAsync("/Customer", dto);
             string result = await response.Content.ReadAsStringAsync();
             if (!response.IsSuccessStatusCode)
@@ -39,5 +47,34 @@ public class CustomerHttpClient : ICustomerService
             throw;
         }
         
+    }
+
+
+    private string dataValidation(string check)
+    {
+        string returnString = check;
+
+        if (returnString.Contains("Ø") || returnString.Contains("ø"))
+        {
+            returnString = returnString.Replace("Ø", "OE");
+            returnString = returnString.Replace("ø", "oe");
+        }
+
+        if (returnString.Contains("Æ") || returnString.Contains("æ"))
+        {
+            returnString = returnString.Replace("Æ", "AE");
+            returnString = returnString.Replace("æ", "ae");
+        }
+        if (returnString.Contains("Å") || returnString.Contains("å"))
+        {
+            returnString = returnString.Replace("Å", "AA");
+            returnString = returnString.Replace("å", "aa");
+        }
+        if (returnString.Contains("+45"))
+        {
+            returnString = returnString.Replace("+", "");
+        }
+
+        return returnString;
     }
 }
